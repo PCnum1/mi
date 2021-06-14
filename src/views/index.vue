94e58e8a6c1a5befd82ed919de89059588d1b680
lv <1284535166@qq.com>
2021-06-14 22:49:33 +0800

    完成广告部分数据渲染

--- src/views/index.vue
+++ src/views/index.vue
@@ -71,13 +71,41 @@
                 </swiper>
             </div>
             <div class="ads-box">
-
+                <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+                    <img :src="item.img">
+                </a>
             </div>
             <div class="banner">
-
+                <a href="/#/product/30">
+                    <img src="/imgs/banner-1.png" alt="">
+                </a>
             </div>
-            <div class="product-box">
-
+        </div>
+        <div class="product-box">
+            <div class="container">
+                <h2>手机</h2>
+                <div class="wrapper">
+                    <div class="banner-left">
+                        <a href="/#/product/35">
+                            <img src="/imgs/mix-alpha.jpg">
+                        </a>
+                    </div>
+                    <div class="list-box">
+                        <div class="list" v-for="(arr,index) in phoneList" :key="index">
+                            <div class="item" v-for="(item,index1) in arr" :key="index1">
+                                <span :class="index1 % 2 === 0? 'new-pro': 'kill-pro'">新品</span>
+                                <div class="item-img">
+                                    <img :src="item.mainImage">
+                                </div>
+                                <div class="item-info">
+                                    <h3>{{item.name}}</h3>
+                                    <p>{{item.subtitle}}</p>
+                                    <p class="price">{{item.price}}元</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <service-bar></service-bar>
@@ -159,15 +187,44 @@ require('swiper/dist/css/swiper.css')
                         },
                         
                     ],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
-                ]
+                ],
+                adsList:[
+                    {
+                        id:33,
+                        img:'/imgs/ads/ads-1.png',
+                    },
+                    {
+                        id:48,
+                        img:'/imgs/ads/ads-2.jpg',
+                    },
+                    {
+                        id:45,
+                        img:'/imgs/ads/ads-3.png',
+                    },
+                    {
+                        id:47,
+                        img:'/imgs/ads/ads-4.jpg',
+                    },
+                ],
+                phoneList:[]
+            }
+        },
+        mounted(){
+            this.init()
+        },
+        methods:{
+            init(){
+                this.axios.get('/products',{params:{categoryId:100012,pageSize:8}}).then((res)=>{
+                    this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)]
+                })
             }
         }
     }
 </script>
 
 <style lang="scss">
-@import './../assets/scss/base.scss';
 @import './../assets/scss/mixin.scss';
+@import './../assets/scss/config.scss';
 .index{
 
     .swiper-box{
@@ -184,7 +241,7 @@ require('swiper/dist/css/swiper.css')
                     height: 50px;
                     line-height: 50px;
                     &:hover{
-                        background-color: #FF6600;
+                        background-color: $colorA;
                         .children{
                             display: block;
                         }
@@ -192,7 +249,7 @@ require('swiper/dist/css/swiper.css')
                     a{  
                         display: block;
                         font-size: 16px;
-                        color: #ffffff;
+                        color: $colorG;
                         padding-left: 30px;
                         position: relative;
                         &:after{
@@ -247,7 +304,99 @@ require('swiper/dist/css/swiper.css')
                 height: 100%;
             }
         }
-
+    }
+    .ads-box{
+        @include flex();
+        margin-top: 14px;
+        margin-bottom: 31px;
+        a{
+            width: 296px;
+            height: 167px;
+        }
+    }
+    .banner{
+        margin-bottom: 50px;
+    }
+    .product-box{
+        background-color: $colorJ;
+        padding: 30px 0 50px;
+        h2{
+            font-size: $fontF;
+            height: 21px;
+            line-height: 21px;
+            color: $colorB;
+            margin-bottom: 20px;
+        }
+        .wrapper{
+            display: flex;
+            .banner-left{
+                margin-right: 16px;
+                img{
+                    width: 224px;
+                    height: 619px;
+                }
+            }
+            .list-box{
+                .list{
+                    @include flex();
+                    width: 986px;
+                    margin-bottom: 14px;
+                    &:last-child{
+                        margin-bottom: 0;
+                    }
+                    .item{
+                        width: 236px;
+                        height: 302px;
+                        background-color: $colorG;
+                        text-align: center;
+                        span{
+                            display: inline-block;
+                            width: 67px;
+                            height: 24px;
+                            line-height: 24px;
+                            color: $colorG;
+                            &.new-pro{
+                                background-color: #7ECF68;
+                            }
+                            &.kill-pro{
+                                background-color: #E82626;
+                            }
+                        }
+                        .item-img{
+                            img{
+                                width: 100%;
+                                height: 195px;
+                            }
+                        }
+                        .item-info{
+                            h3{
+                                color: $colorB;
+                                font-size: $fontJ;
+                                line-height: $fontJ;
+                                font-weight: bold;
+                            }
+                            p{
+                                color: $colorD;
+                                line-height: 13px;
+                                margin: 6px auto 13px;
+                            }
+                            .price{
+                                color: #F20A0A;
+                                font-size: $fontJ;
+                                font-weight: bold;
+                                cursor: pointer;
+                                &:after{
+                                    content:'';
+                                    @include backImg(22px,22px,'/imgs/icon-cart-hover.png');
+                                    margin-left: 5px;
+                                    vertical-align: middle;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 </style>
